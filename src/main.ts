7eff4c48a21dfc3086ee5a6e4172a20091eea1e9
Minecodes <65566012+Minecodes@users.noreply.github.com>
2021-10-28 13:15:11 +0200

    Delete main.ts

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-function fox() {
-    return `https://randomfox.ca/images/${Math.floor((Math.random() * 122) + 1)}.jpg`
-}
-
-function customfox(urls: Array<String>) {
-    return urls[Math.floor((Math.random() * urls.length))];
-}
-
-export { fox, customfox };
